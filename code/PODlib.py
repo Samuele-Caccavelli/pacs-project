00c99c29377333ae21e4479da0321ab28ade929c
Samuele Caccavelli <samuele.caccavelli@mail.polimi.it>
2025-11-05 14:10:23 +0100

    Initial commit of existing project

---
+++ code/PODlib.py
@@ -0,0 +1,214 @@
+import torch
+from scipy.linalg import svd
+import numpy as np
+from dlroms import euclidean
+import matplotlib.pyplot as plt
+
+def omega_weights(theta, theta_i, lambda_penalty = 1e-2, pbad_index_list=None):
+    """Compute an exponentially decaying weight based on the squared Euclidean distance between two parameter vectors.
+    The weight follows a Gaussian-like decay:
+        w = exp(-‖θ - θᵢ‖² / λ_penalty²)
+    where the distance is computed only over the indices specified in `pbad_index_list`.
+    Closer vectors yield higher weights.
+    
+    Input:
+            theta               (torch.Tensor)              First parameter vector.
+            theta_i             (torch.Tensor)              Second parameter vector.
+            lambda_penalty      (float, optional)           Scaling parameter controlling the rate of exponential decay.
+                                                            Defaults to 1e-2.
+            pbad_index_list     (list of int, optional)     Indices specifying the components of the vectors to consider in the distance computation.
+                                                            Defaults to None - in this case all components of the vectors are used.
+    
+    Output:
+            (float) Exponentially decaying weight.
+    """
+    if(pbad_index_list == None):
+        pbad_index_list = list(range(theta.size(0)))
+    
+    return np.exp((-euclidean(theta[pbad_index_list]-theta_i[pbad_index_list], squared=True)/lambda_penalty**2).item())
+
+class weighted_POD:
+    """Class implementing the weighted POD method.
+    
+    Attributes:
+            A               (torch.Tensor)              Ambient space.
+            U               (torch.Tensor)              Tensor containing the dataset.
+            theta_full      (torch.Tensor)              Tensor containing the parameters related to the dataset.
+            #! should i put the shape (in terms of n_parameters, n_snapshots_considered)?
+            n_basis         (int)                       Number of basis to be returned.
+            omega_func      (function)                  Function to compute the weights.
+                                                        It must have the signature:
+                                                            omega_func(theta: torch.Tensor, theta_i: torch.Tensor) -> float
+            trainable       (bool)                      Bool value describing if working in the ambient space or in the full space.
+                                                        When True, the returned space is considered inside the ambient space.
+
+    Private attributes:
+            __s_values      (ndarray)                   The singular values, sorted in non-increasing order.
+    """
+
+    def __init__(self, A, U, theta_full, n_basis, omega_func):
+        """Initialize an weighted_POD object.
+        
+        Input:
+                A               (torch.Tensor)              Ambient space.
+                U               (torch.Tensor)              Tensor containing the dataset.
+                theta_full      (torch.Tensor)              Tensor containing the parameters related to the dataset.
+                #! should i put the shape (in terms of n_parameters, n_snapshots_considered)?
+                n_basis         (int)                       Number of basis to be returned.
+                omega_func      (function)                  Function to compute the weights.
+                                                            It must have the signature:
+                                                                omega_func(theta: torch.Tensor, theta_i: torch.Tensor) -> float
+        """
+        
+        self.A = A
+        self.U = U
+        self.theta_full = theta_full
+        self.n_basis = n_basis
+        self.omega_func = omega_func
+        self.trainable = True
+        self.__s_values = None
+
+    def compute_space(self, theta):
+        #! change name?
+        """Compute the basis related to a specific theta.
+        
+        Input:
+                theta           (torch.Tensor)              Parameter vector to consider when computing the space.
+
+        Output:
+                (torch.Tensor) Basis related to a specific theta.
+        """
+        W = torch.empty(self.U.shape[0], self.U.shape[1])
+
+        if(theta.ndim != 1):
+            theta.squeeze(0)
+
+        for iter in range(self.U.shape[1]):
+            W[:,iter] = self.U[:,iter] * self.omega_func(theta, self.theta_full[:,iter])
+
+        X,self.__s_values,_ = svd(W, full_matrices=False)
+
+        return torch.from_numpy(X[:,:self.n_basis])
+
+    def __call__(self, theta):
+        """Returns the basis related to a specific theta.
+        
+        Input:
+                theta           (torch.Tensor)              Parameter vector to consider when computing the space.
+
+        Output:
+                (torch.Tensor) Basis related to a specific theta. If `trainable` is set to True, the basis returned refers to the ambient space, otherwise it refers to the full space.
+        """
+        if(theta.ndim == 1):
+            out = self.compute_space(theta)
+            out = torch.t(out)
+            return out if self.trainable else out.matmul(self.A)
+        elif(theta.ndim == 2):
+            return torch.stack([self.__call__(th) for th in theta], axis = 0)
+        else:
+            raise RuntimeError("Wrong shape.")
+
+    def singular_values(self):
+        """Returns the singular values related to the basis computation of a specific theta. It refers to the singular values computed in the last call of `compute_space`.
+
+        Output:
+                (ndarray) The singular values, sorted in non-increasing order.
+        """
+        return self.__s_values
+
+    def freeze(self):
+        """Needed for symmetry with the DOD class. Set `trainable` to False.
+        """
+        self.trainable = False
+
+    def unfreeze(self):
+        """Needed for symmetry with the DOD class. Set `trainable` to True.
+        """
+        self.trainable = True
+
+def n_choice_graphs(s_values, N_max=None, n_trajectories=None, which='all', figsize=(16,16)):
+    """Plots graphs needed to chose the right number of basis.
+        
+    Input:
+            s_values            (numpy.Tensor)              Matrix containing the singular values for different values of theta.
+            #! is it ok that i have everywhere a torch Tensor but here?
+            #! also, the size
+            N_max               (int, optional)             Number of basis to plot in all the graphs.
+                                                            Defaults to None - in this case, all basis are plotted.
+            n_trajectories      (int, optional)             Number of trajectories to plot in the graph type 'trajectories'.
+                                                            Defaults to None - in this case, all trajectories are plotted.
+            which               (string, optional)          Which graphs to show. Choices are:
+                                                            - 'delta'           : graph of the relative differences of the singular values
+                                                            #! fix this
+                                                            - 'range'           : graph of the range between the minimum and maximum singular value for each number of basis
+                                                            - 'trajectories'    : graph of the trajectories of the singular values for each theta.
+                                                            - 'all'             : all of the above.
+                                                            Defaults to 'all'.
+            figsize             (float, float)              Width and height of the plot in inches.
+                                                            Defaults to (16,16).
+    """
+    # here the expected input is a tensor of size (n1, n2) where
+    # n1 is the number of that_i for which the singular values have been computed
+    # n2 = min(nA, n1) with nA = number of basis of the ambient space
+
+    if(N_max==None):
+        N_max = s_values.shape[1]
+
+    if(n_trajectories==None):
+        n_trajectories = s_values.shape[0]
+
+    relative_diff = (s_values[:, :-1] - s_values[:, 1:])/s_values[:, :-1]
+
+    delta_n_mean = (relative_diff).mean(axis=0)
+    delta_n_min = (relative_diff).min(axis=0)
+    min_n = s_values.min(axis = 0)
+    max_n = s_values.max(axis = 0)
+
+    x = np.arange(1, N_max+1)
+
+    plots_to_show = []
+    if which in ['all', 'delta']:
+        plots_to_show.append('delta')
+    if which in ['all', 'range']:
+        plots_to_show.append('range')
+    if which in ['all', 'trajectories']:
+        plots_to_show.append('trajectories')
+
+    n_subplots = len(plots_to_show)
+    if n_subplots == 0:
+        raise ValueError("Invalid 'which' argument, nothing to plot.")
+
+    fig, axes = plt.subplots(n_subplots, 1, figsize=figsize, sharex=True)
+
+    if n_subplots == 1:
+        axes = [axes]
+
+    for ax, plot_name in zip(axes, plots_to_show):
+        if plot_name == 'delta':
+            ax.loglog(x[:-1], delta_n_mean[:N_max-1], marker="o", color='orange', label='Mean')
+            ax.loglog(x[:-1], delta_n_min[:N_max-1], marker="*", color='blue', label='Minimum')
+            ax.legend()
+            ax.set_title('δ(n)')
+            ax.grid(True)
+        
+        elif plot_name == 'range':
+            ax.fill_between(x, min_n[:N_max], max_n[:N_max], color='blue', alpha=0.2)
+            ax.set_xscale('log')
+            ax.set_yscale('log')
+            ax.set_title('Singular Values Range')
+            ax.grid(True)
+        
+        elif plot_name == 'trajectories':
+            ax.loglog(x, s_values.T[:N_max, :n_trajectories])
+            ax.set_title('Trajectories')
+            ax.grid(True)
+
+    ticks = np.arange(N_max//4) * 4 + 1
+    axes[-1].set_xticks(ticks)
+    axes[-1].set_xticklabels(ticks)
+    axes[-1].set_xlabel('Number of basis')
+
+    plt.tight_layout()
+    plt.show()
+
+    return
---
+++ code/variabilitylib.py
@@ -0,0 +1,223 @@
+from dlroms import *
+from dodlib import DOD
+import torch
+
+from tqdm import tqdm
+import numpy as np
+from scipy.optimize import minimize
+import random
+
+
+def IdentityScaling(theta):
+    """This function performs no transformation on the input. It is useful as a default scaling operator.
+    
+    Input:
+            theta               (torch.Tensor)              Input data.
+    
+    Output:
+            (torch.Tensor) Unchanged input.
+    """
+    return theta
+
+def Frobenius_metric(V1, V2):
+    """Compute the Frobenius distance between the projection matrices associated with two basis.
+
+    The metric is defined as:
+        d = || V₁V₁ᵀ - V₂V₂ᵀ ||_F
+    where ||·||_F denotes the Frobenius norm.
+
+    Input:
+            V1              (torch.Tensor)              First basis matrix.
+            V2              (torch.Tensor)              Second basis matrix.
+
+    Output:
+            (torch.Tensor) Scalar tensor representing the Frobenius distance.
+    """
+
+    projection1 = torch.matmul(V1, torch.t(V1))
+    projection2 = torch.matmul(V2, torch.t(V2))
+
+    return torch.norm(projection1-projection2, p='fro')
+
+class LocalBasis():
+    """LocalBasis class, used for computing the quantity K via different methods.
+    #! is it really called K?
+    
+    Attributes:
+            q               (int)                       Dimensions of the parameter space.
+            pbad_index_list (list of int)               List of indexes corresponding to the bad parameters.
+            #! maybe we can call it p_prime instead of pbad to be more coherent with the paper
+            module          (function)                  A map from Θ to R^(n_basis, Na).
+                                                        #! specify what these are?
+                                                        It must have the signature:
+                                                            module(theta: torch.Tensor) -> torch.Tensor
+            scaling         (function)                  A map from [0,1]^q to Θ * Θ'.
+                                                        It must have the signature:
+                                                            scaling(theta: torch.Tensor) -> torch.Tensor
+            metric          (function)                  Function between two basis spaces to compute a metric returned as a scalar tensor.
+                                                        It must have the signature:
+                                                            metric(V1: torch.Tensor, V2: torch.Tensor) -> torch.Tensor
+    """
+
+    def __init__(self, q, pbad_index_list, module, scaling = IdentityScaling, metric = Frobenius_metric):
+        """Initialize an LocalBasis object.
+        
+        Attributes:
+                q               (int)                       Dimensions of the parameter space.
+                pbad_index_list (list of int)               List of indexes corresponding to the bad parameters.
+                #! maybe we can call it p_prime instead of pbad to be more coherent with the paper
+                module          (function)                  A map from Θ to R^(n_basis, Na).
+                                                            #! specify what these are?
+                                                            It must have the signature:
+                                                                module(theta: torch.Tensor) -> torch.Tensor
+                scaling         (function)                  A map from [0,1]^q to Θ * Θ'.
+                                                            It must have the signature:
+                                                                scaling(theta: torch.Tensor) -> torch.Tensor
+                                                            Defaults to `IdentityScaling`
+                metric          (function)                  Function between two basis spaces to compute a metric returned as a scalar tensor.
+                                                            It must have the signature:
+                                                                metric(V1: torch.Tensor, V2: torch.Tensor) -> torch.Tensor
+                                                            Defaults to `Frobenius_metric`
+        """
+        
+        self.q = q
+        self.pbad_index_list = pbad_index_list
+        self.module = module
+        self.scaling = scaling
+        self.metric = metric
+
+        # We freeze the module, so that it is not anymore trainable (needed for module based on Neural Networks)
+        # We then set its `trainable` attribute to True so that we will work in the ambient space
+        self.module.freeze()
+        self.module.trainable = True
+
+    def __call__(self, theta):
+        # Here we are considering a theta inside [0,1]^q
+        ##! maybe we should put here a check on theta being inside the right interval
+        ##! but this could slow down this function that is then called by all the others
+        # So we first rescale it to be inside Θ * Θ', then we compute the basis
+        params = self.scaling(theta)
+        return self.module(params[self.pbad_index_list].unsqueeze(0)).squeeze()
+
+    # Private method needed for K_h_j
+    def __CheckH(self, theta_hat_j, h):
+        # Checks if 0 <= theta_hat_j + h <= 1
+        return (h < -theta_hat_j) and (h > 1-theta_hat_j)
+
+    def K_h_j(self, j, theta_hat, h=1e-1):
+        if(j<0 or j>theta_hat.size()[0]-1):
+            raise RuntimeError("The given j is out of bound for the dimensions of theta")
+        if(theta_hat[j] < 0 or theta_hat[j] > 1):
+            raise RuntimeError("Theta hat should be normalized first")
+        if(self.__CheckH(theta_hat[j], h)):
+            raise RuntimeError("The given h for the computation of K_h_j make theta_hat go out of its space")
+
+        theta_hat_var = theta_hat.clone()
+        theta_hat_var[j] += h
+
+        # We compute the two basis spaces
+        V_theta_hat = self.__call__(theta_hat)
+        V_theta_hat_var = self.__call__(theta_hat_var)
+
+        distance = self.metric(V_theta_hat, V_theta_hat_var).item()
+
+        return distance / abs(h)
+
+    def K_sup_j(self, j, theta_hat):
+        if(j<0 or j>theta_hat.size()[0]-1):
+            raise RuntimeError("The given j is out of bound for the dimensions of theta")
+        if(theta_hat[j] < 0 or theta_hat[j] > 1):
+            raise RuntimeError("Theta hat should be normalized first")
+
+        def objective(h):
+            ##! if we use self.K_h_j here, we don't have to write twice the same things
+            ##! but in this way we are doing the checks at the start of self.K_h_j each time the optimization algorithm
+            ##! calls this function
+            # Attention here: we need to return (-K_h_j)
+            return -(self.K_h_j(j, theta_hat, h[0]))
+
+        # def constraint1(h):
+        #     return h + theta_hat[j].item()
+
+        # def constraint2(h):
+        #     return 1-h-theta_hat[j].item()
+
+        # constraints = [
+        #     {'type': 'ineq', 'fun': constraint1},
+        #     {'type': 'ineq', 'fun': constraint2}
+        # ]
+
+        bounds = [(-theta_hat[j].item(), 1 - theta_hat[j].item())]
+
+        # h0 is the medium point between [-theta_hat[j], 1-theta_hat[j]]
+        h0 = np.array(0.5 - theta_hat[j].item())
+
+        # result = minimize(objective, h0, constraints=constraints, method="SQLSQP")
+        result = minimize(objective, h0, bounds=bounds, method="Powell")
+
+        print("The optimizer had success? ", result.success, " and it said ", result.message)
+        print("At h = ", result.x, " the values is " , result.fun)
+
+        # We are interested in the maximum, so we return (-minimum)
+        return -result.fun
+
+    # Private method
+    # Monte Carlo estimate of K_sup_j
+    def __K_sup_j_tot(self, j, S, verbose, theta_dataset):
+        sum = 0
+
+        if(theta_dataset == None):
+            print(S, "random values of the parameters will be generated for the Monte Carlo estimate")
+            theta_dataset = torch.rand(S, self.q)
+
+        else:
+            S = theta_dataset.size()[0]
+            print(S, "non-random values of the parameters were given, those will be used for the Monte Carlo estimate")
+
+        K_vector = []
+
+        for iter in tqdm(range(S), disable=not verbose, desc="Monte Carlo Estimate progress"):
+            theta_hat = theta_dataset[iter]
+            value = self.K_sup_j(j, theta_hat)
+            K_vector.append(value)
+
+        return np.mean(K_vector), np.std(K_vector)
+
+    # Private method
+    # Monte Carlo estimate of K_h_j
+    def __K_h_j_tot(self, j, h, S, verbose, theta_dataset):
+        sum = 0
+
+        if(theta_dataset == None):
+            print(S, "random values of the parameters will be generated for the Monte Carlo estimate")
+            theta_dataset = torch.rand(S, self.q)
+
+        else:
+            S = theta_dataset.size()[0]
+            print(S, "non-random values of the parameters were given, those will be used for the Monte Carlo estimate")
+
+        K_vector = []
+
+        for iter in tqdm(range(S), disable=not verbose, desc="Monte Carlo Estimate progress"):
+            theta_hat = theta_dataset[iter]
+            
+            # We transform the j component of theta_hat to ensure |h| < theta_hat[j] < 1-|h|, so that 
+            # we won't have errors while computing K_h_j
+            theta_hat[j] = theta_hat[j] * (1-2*abs(h)) + abs(h)
+
+            # We compute K_h_J 50% of the times in direction h, 50% in direction -h
+            if random.choice([True, False]):
+                K_vector.append(self.K_h_j(j, theta_hat, h))
+            else:
+                K_vector.append(self.K_h_j(j, theta_hat, -h))
+
+        return np.mean(K_vector), np.std(K_vector)
+
+    # Public method
+    # Wrapper for the two private methods
+    def K_j_tot(self, j, h=None, S=1000, verbose = False, theta_dataset = None):
+        if(h==None):
+            return self.__K_sup_j_tot(j, S, verbose, theta_dataset)
+        return self.__K_h_j_tot(j, h, S, verbose, theta_dataset)
+
+    
